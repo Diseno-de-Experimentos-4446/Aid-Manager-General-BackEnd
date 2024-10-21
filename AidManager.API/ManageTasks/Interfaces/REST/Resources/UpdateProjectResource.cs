84530fb4faab30dd6e52627dfbd8309391ad5ff8
SEBRH <u202211894@upc.edu.pe>
2024-10-21 12:57:31 -0500

    fix(minorFix): minor mistakes fixed

--- AidManager.API/ManageTasks/Interfaces/REST/Resources/UpdateProjectResource.cs
+++ AidManager.API/ManageTasks/Interfaces/REST/Resources/UpdateProjectResource.cs
@@ -1,3 +1,9 @@
 ﻿namespace AidManager.API.ManageTasks.Interfaces.REST.Resources;
 
-public record UpdateProjectResource(string Name, string Description, List<string> ImageUrl, int CompanyId, DateOnly ProjectDate, DateTime ProjectTime, string ProjectLocation);
+public record UpdateProjectResource(string Name,
+    string Description,
+    List<string> ImageUrl,
+    int CompanyId,
+    DateOnly ProjectDate,
+    TimeOnly ProjectTime,
+    string ProjectLocation);
