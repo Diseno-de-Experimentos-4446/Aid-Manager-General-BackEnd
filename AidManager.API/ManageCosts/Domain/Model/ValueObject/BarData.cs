1249f36e0233ac9c25316e2db1292be18a595e85
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-10-21 10:24:08 -0500

    update(important): minor webas

--- AidManager.API/ManageCosts/Domain/Model/ValueObject/BarData.cs
+++ AidManager.API/ManageCosts/Domain/Model/ValueObject/BarData.cs
@@ -2,11 +2,11 @@
 
 public class BarData
 {
-    public double SunAmmount { get; set; }
-    public double MonAmmount { get; set; }
-    public double TueAmmount { get; set; }
-    public double WedAmmount { get; set; }
-    public double ThuAmmount { get; set; }
-    public double FriAmmount { get; set; }
-    public double SatAmmount { get; set; }
+    public double SunAmount { get; set; }
+    public double MonAmount { get; set; }
+    public double TueAmount { get; set; }
+    public double WedAmount { get; set; }
+    public double ThuAmount { get; set; }
+    public double FriAmount { get; set; }
+    public double SatAmount { get; set; }
 }
--- AidManager.API/ManageCosts/Domain/Model/ValueObject/LineChartData.cs
+++ AidManager.API/ManageCosts/Domain/Model/ValueObject/LineChartData.cs
@@ -4,4 +4,9 @@ public class LineChartData
 {
     public double Data1 { get; set; } 
     public double Data2 { get; set; }
+    public double Data3 { get; set; }
+    public double Data4 { get; set; }
+    public double Data5 { get; set; }
+    public double Data6 { get; set; }
+    public double Data7 { get; set; }
 }
