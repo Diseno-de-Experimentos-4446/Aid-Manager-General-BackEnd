f6fa0d3b0fdf7b8086dce6642367e09ee780a212
SEBRH <u202211894@upc.edu.pe>
2024-11-13 11:57:26 -0500

    fix(Post): Fixed Post And Comment Resources

--- AidManager.API/Collaborate/Interfaces/REST/Resources/CommentResource.cs
+++ AidManager.API/Collaborate/Interfaces/REST/Resources/CommentResource.cs
@@ -1,4 +1,4 @@
 ﻿namespace AidManager.API.Collaborate.Interfaces.REST.Resources;
 
 public record CommentResource
-(int Id, string Comment, int UserId, string UserName, string userEmail ,string userImage, int PostId, string CommentTime);
+(int Id, string Comment, int UserId, string UserName, string UserEmail ,string UserImage, int PostId, string CommentTime);
