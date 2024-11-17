04870818363052865c4ccc29b395fd0e2e859d48
Nicolas Esteban <120282675+Nekolas777@users.noreply.github.com>
2024-11-17 17:05:19 -0500

    fix: resource field name for request

--- AidManager.API/ManageTasks/Interfaces/REST/Resources/UpdateTaskItemResource.cs
+++ AidManager.API/ManageTasks/Interfaces/REST/Resources/UpdateTaskItemResource.cs
@@ -1,3 +1,3 @@
 ﻿namespace AidManager.API.ManageTasks.Interfaces.REST.Resources;
 
-public record UpdateTaskItemResource(string Title, string Description, DateOnly DueDate, string State, int UserId);
+public record UpdateTaskItemResource(string Title, string Description, DateOnly DueDate, string State, int assigneeId);
--- AidManager.API/ManageTasks/Interfaces/REST/Transform/UpdateTaskItemCommandFromResourceAssembler.cs
+++ AidManager.API/ManageTasks/Interfaces/REST/Transform/UpdateTaskItemCommandFromResourceAssembler.cs
@@ -6,6 +6,6 @@ namespace AidManager.API.ManageTasks.Interfaces.REST.Transform;
 public static class UpdateTaskItemCommandFromResourceAssembler
 {
     public static UpdateTaskCommand ToCommandFromResource(UpdateTaskItemResource resource, int taskId ,int projectId) =>
-        new UpdateTaskCommand(taskId,resource.Title, resource.Description, resource.DueDate, resource.State, resource.UserId, projectId);
+        new UpdateTaskCommand(taskId,resource.Title, resource.Description, resource.DueDate, resource.State, resource.assigneeId, projectId);
 
 }
