27b9453c43c596c27869c579ffd93818f6a96180
U202210138 <u202210138@upc.edu.pe>
2025-05-10 19:22:30 -0500

    feat: added Unit Test CreateProject_WithValidCommand

--- AidManager.API/Tests/UnitTests/ProjectTests.cs
+++ AidManager.API/Tests/UnitTests/ProjectTests.cs
@@ -26,6 +26,36 @@ namespace AidManager.Tests.CoreEntities
             Assert.That(project.Rating, Is.EqualTo(4.5));
         }
 
+        [Test]
+        public void CreateProject_WithValidCommand_ShouldSetAllProperties()
+        {
+            // Arrange
+            var command = new CreateProjectCommand(
+                "Test Project",
+                "Project Description",
+                new List<string> { "image1.jpg" },
+                1,
+                "2023-10-15",
+                "14:30",
+                "Test Location"
+            );
+
+            // Act
+            var project = new Project(command);
+
+            // Assert
+            Assert.That(project.Name, Is.EqualTo("Test Project"));
+            Assert.That(project.Description, Is.EqualTo("Project Description"));
+            Assert.That(project.ImageUrl.Count, Is.EqualTo(1));
+            Assert.That(project.ImageUrl[0].Url, Is.EqualTo("image1.jpg"));
+            Assert.That(project.CompanyId, Is.EqualTo(1));
+            Assert.That(project.ProjectDate, Is.EqualTo(DateOnly.Parse("2023-10-15")));
+            Assert.That(project.ProjectTime, Is.EqualTo(TimeOnly.Parse("14:30")));
+            Assert.That(project.ProjectLocation, Is.EqualTo("Test Location"));
+            Assert.That(project.Rating, Is.EqualTo(0.00));
+            Assert.That(project.AuditDate, Is.EqualTo(DateOnly.FromDateTime(DateTime.Now)));
+        }
+
         
     }
 }
